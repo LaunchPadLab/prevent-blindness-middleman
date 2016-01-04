f1fce5cbbf180744d767ec316a493fe6f701924e
Monique Marchwiany <monique@launchpadlab.com>
2016-01-04 09:29:05 -0600

    base styles in there

---
+++ source/javascripts/page.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+     $('.menuTrigger').click( function () {
+        $('.panel').toggleClass('isOpen');
+       $('.wrapper').toggleClass('pushed');
+    });
+
+    $('.openSubPanel').click( function() {
+        $(this).next('.subPanel').addClass('isOpen');
+    });
+
+    $('.closeSubPanel').click( function() {
+        $('.subPanel').removeClass('isOpen');
+    });
+
+    $('.closePanel').click( function() {
+        $('.panel').removeClass('isOpen');
+        $('.wrapper').removeClass('pushed');
+    });
+ });
